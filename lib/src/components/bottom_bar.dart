c561bb088436fcbf3705194233728be72cd3aafd
qucals <totalboy@outlook.com>
2022-03-31 01:58:34 +0500

    Добавил нижний бар
    Signed-off-by: qucals <totalboy@outlook.com>

---
+++ lib/src/components/bottom_bar.dart
@@ -0,0 +1,194 @@
+/*
+ Copyright 2022 qucals / https://github.com/qucals
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import 'package:flutter/material.dart';
+
+class LibraryAnimatedBottomBar extends StatelessWidget {
+  LibraryAnimatedBottomBar({
+    Key? key,
+    this.selectedIndex = 0,
+    this.showElevation = true,
+    this.iconSize = 24,
+    this.backgroundColor,
+    this.activeBackgroundColor,
+    this.itemCornerRadius = 50,
+    this.containerHeight = 56,
+    this.containerCornerRadius = 0,
+    this.animationDuration = const Duration(milliseconds: 270),
+    this.mainAxisAlignment = MainAxisAlignment.spaceBetween,
+    required this.items,
+    required this.onItemSelected,
+    this.curve = Curves.linear,
+  })  : assert(items.length >= 2 && items.length <= 5),
+        super(key: key);
+
+  final int selectedIndex;
+  final double iconSize;
+  final Color? backgroundColor;
+  final Color? activeBackgroundColor;
+  final bool showElevation;
+  final Duration animationDuration;
+  final List<BottomNavyBarItem> items;
+  final ValueChanged<int> onItemSelected;
+  final MainAxisAlignment mainAxisAlignment;
+  final double itemCornerRadius;
+  final double containerHeight;
+  final double containerCornerRadius;
+  final Curve curve;
+
+  @override
+  Widget build(BuildContext context) {
+    final bgColor = backgroundColor ?? Theme.of(context).bottomAppBarColor;
+    final activeBgColor =
+        activeBackgroundColor ?? Theme.of(context).colorScheme.secondary;
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.all(Radius.circular(containerCornerRadius)),
+        boxShadow: [
+          if (showElevation)
+            const BoxShadow(
+              color: Colors.black12,
+              blurRadius: 2,
+            ),
+        ],
+      ),
+      child: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: containerHeight,
+          padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 30),
+          child: Row(
+            mainAxisAlignment: mainAxisAlignment,
+            children: items.map((item) {
+              var index = items.indexOf(item);
+              return GestureDetector(
+                onTap: () => onItemSelected(index),
+                child: _ItemWidget(
+                  item: item,
+                  iconSize: iconSize,
+                  isSelected: index == selectedIndex,
+                  backgroundColor: bgColor,
+                  activeBackgroundColor: activeBgColor,
+                  itemCornerRadius: itemCornerRadius,
+                  animationDuration: animationDuration,
+                  curve: curve,
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ItemWidget extends StatelessWidget {
+  final double iconSize;
+  final bool isSelected;
+  final BottomNavyBarItem item;
+  final Color backgroundColor;
+  final Color activeBackgroundColor;
+  final double itemCornerRadius;
+  final Duration animationDuration;
+  final Curve curve;
+
+  const _ItemWidget({
+    Key? key,
+    required this.item,
+    required this.isSelected,
+    required this.backgroundColor,
+    required this.activeBackgroundColor,
+    required this.animationDuration,
+    required this.itemCornerRadius,
+    required this.iconSize,
+    this.curve = Curves.linear,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      container: true,
+      selected: isSelected,
+      child: AnimatedContainer(
+        width: isSelected ? item.selectedWidth : 50,
+        height: double.maxFinite,
+        duration: animationDuration,
+        curve: curve,
+        decoration: BoxDecoration(
+          color: isSelected ? activeBackgroundColor : backgroundColor,
+          borderRadius: BorderRadius.circular(itemCornerRadius),
+        ),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          physics: NeverScrollableScrollPhysics(),
+          child: Container(
+            width: isSelected ? 170 : 50,
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                IconTheme(
+                  data: IconThemeData(
+                    size: iconSize,
+                    color: isSelected
+                        ? item.activeColor.withOpacity(1)
+                        : item.inactiveColor ?? item.activeColor,
+                  ),
+                  child: item.icon,
+                ),
+                if (isSelected)
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: DefaultTextStyle.merge(
+                      style: TextStyle(
+                        color: item.activeColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      maxLines: 1,
+                      textAlign: item.textAlign,
+                      child: item.title,
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BottomNavyBarItem {
+  BottomNavyBarItem({
+    required this.icon,
+    required this.title,
+    this.activeColor = Colors.blue,
+    this.textAlign,
+    this.inactiveColor,
+    this.selectedWidth = 160,
+  });
+
+  final Widget icon;
+  final Widget title;
+  final Color activeColor;
+  final Color? inactiveColor;
+  final TextAlign? textAlign;
+  final double selectedWidth;
+}
--- lib/src/main_page/main_page.dart
+++ lib/src/main_page/main_page.dart
@@ -16,6 +16,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:government_library/src/components/bottom_bar.dart';
 import 'package:group_button/group_button.dart';
 
 import 'package:government_library/src/components/text_button.dart';
@@ -33,6 +34,8 @@ class LibraryMainPage extends StatefulWidget {
 }
 
 class _LibraryMainPageState extends State<LibraryMainPage> {
+  int _currentPageIndex = 0;
+
   final TextEditingController _search = TextEditingController();
   final GroupButtonController _genres = GroupButtonController(selectedIndex: 0);
 
@@ -50,200 +53,228 @@ class _LibraryMainPageState extends State<LibraryMainPage> {
     selectedGenre = genres[_genres.selectedIndex!];
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
-          child: Column(
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.only(top: 10),
-                alignment: Alignment.center,
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                      'Привет, Кирилл',
-                      style:
-                          widget.themeNotifier.getTheme().textTheme.headline5,
-                    ),
-                    const Spacer(
-                      flex: 1,
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: SvgPicture.asset(
-                        'assets/images/icons/settings_icon.svg',
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
-                alignment: Alignment.center,
-                child: LibraryTextField(
-                  controller: _search,
-                  hintText: 'название книги, автор или жанр',
-                  onSubmitted: (_) => FocusScope.of(context).unfocus(),
-                  labelStyle:
-                      widget.themeNotifier.getTheme().textTheme.overline!,
-                  prefixIcon: SvgPicture.asset(
-                    'assets/images/icons/search_icon.svg',
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Популярные жанры',
-                  style: widget.themeNotifier.getTheme().textTheme.headline6,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                child: GroupButton(
-                  onSelected: (index, isSelected) => setState(() {
-                    selectedGenre = genres[index];
-                  }),
-                  buttons: genres,
-                  controller: _genres,
-                  options: GroupButtonOptions(
-                    spacing: 10,
-                    runSpacing: 5,
-                    borderRadius: BorderRadius.circular(10),
-                    selectedColor:
-                        widget.themeNotifier.getTheme().colorScheme.secondary,
-                    unselectedColor: const Color(0xFFE5E5EA),
-                    mainGroupAlignment: MainGroupAlignment.start,
-                    selectedShadow: [],
-                    unselectedShadow: [],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Жанр "$selectedGenre"',
-                  style: widget.themeNotifier.getTheme().textTheme.headline6,
-                ),
-              ),
-              Container(
-                height: 250,
-                alignment: Alignment.centerLeft,
-                padding: const EdgeInsets.only(top: 20),
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  physics: const ClampingScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 5,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      padding: index == 0 ? null : const EdgeInsets.only(left: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: const Image(
-                              height: 190,
-                              image: AssetImage('assets/images/main_page/book_image.jpg')
-                            )
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          const Text(
-                            'Кот в шляпе',
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 5),
-                          const Text(
-                            'Доктор Сьюз',
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Сейчас в тренде',
-                  style: widget.themeNotifier.getTheme().textTheme.headline6,
-                ),
-              ),
-              Container(
-                height: 250,
-                alignment: Alignment.centerLeft,
-                padding: const EdgeInsets.only(top: 20),
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  physics: const ClampingScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 5,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      padding: index == 0 ? null : const EdgeInsets.only(left: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: const Image(
-                              height: 190,
-                              image: AssetImage('assets/images/main_page/book_image.jpg')
-                            )
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          const Text(
-                            'Кот в шляпе',
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 5),
-                          const Text(
-                            'Доктор Сьюз',
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                ),
-              ),
-            ],
+      body: getBody(),
+      bottomNavigationBar: _buildBottomBar(),
+    );
+  }
+
+  Widget _buildBottomBar() {
+    return LibraryAnimatedBottomBar(
+      containerHeight: 70,
+      containerCornerRadius: 20,
+      backgroundColor: widget.themeNotifier.getTheme().primaryColor,
+      selectedIndex: _currentPageIndex,
+      showElevation: true,
+      itemCornerRadius: 20,
+      curve: Curves.easeIn,
+      onItemSelected: (index) => setState(() => _currentPageIndex = index),
+      items: <BottomNavyBarItem>[
+        BottomNavyBarItem(
+          icon: SvgPicture.asset(
+            'assets/images/icons/search_icon.svg',
+            color: Colors.white,
           ),
+          title: Text('Главная'),
+          activeColor: Colors.white,
+          inactiveColor: Colors.black,
+          textAlign: TextAlign.center,
+          selectedWidth: 105,
+        ),
+        BottomNavyBarItem(
+          icon: SvgPicture.asset(
+            'assets/images/icons/library_icon.svg',
+            color: Colors.white,
+          ),
+          title: Text('Библиотека'),
+          activeColor: Colors.white,
+          inactiveColor: Colors.black,
+          textAlign: TextAlign.center,
+        ),
+        BottomNavyBarItem(
+          icon: SvgPicture.asset(
+            'assets/images/icons/user_icon.svg',
+            color: Colors.white,
+          ),
+          title: Text('Пользователь'),
+          activeColor: Colors.white,
+          inactiveColor: Colors.black,
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  Widget getBody() {
+    List<Widget> pages = [
+      _getMainPage(),
+      _getMainPage(),
+      _getMainPage(),
+    ];
+
+    return IndexedStack(
+      index: _currentPageIndex,
+      children: pages,
+    );
+  }
+
+  Widget _getMainPage() {
+    return SingleChildScrollView(
+      child: Container(
+        alignment: Alignment.center,
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.fromLTRB(30, 60, 30, 30),
+              alignment: Alignment.center,
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'Привет, Кирилл',
+                    style: widget.themeNotifier.getTheme().textTheme.headline5,
+                  ),
+                  const Spacer(
+                    flex: 1,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: SvgPicture.asset(
+                      'assets/images/icons/settings_icon.svg',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: LibraryTextField(
+                controller: _search,
+                hintText: 'название книги, автор или жанр',
+                onSubmitted: (_) => FocusScope.of(context).unfocus(),
+                labelStyle: widget.themeNotifier.getTheme().textTheme.overline!,
+                prefixIcon: SvgPicture.asset(
+                  'assets/images/icons/search_icon.svg',
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Text(
+                'Популярные жанры',
+                style: widget.themeNotifier.getTheme().textTheme.headline6,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: GroupButton(
+                onSelected: (index, isSelected) => setState(() {
+                  selectedGenre = genres[index];
+                }),
+                buttons: genres,
+                controller: _genres,
+                options: GroupButtonOptions(
+                  spacing: 10,
+                  runSpacing: 5,
+                  borderRadius: BorderRadius.circular(10),
+                  selectedColor:
+                      widget.themeNotifier.getTheme().colorScheme.secondary,
+                  unselectedColor: const Color(0xFFE5E5EA),
+                  mainGroupAlignment: MainGroupAlignment.start,
+                  selectedShadow: [],
+                  unselectedShadow: [],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Text(
+                'Жанр "$selectedGenre"',
+                style: widget.themeNotifier.getTheme().textTheme.headline6,
+              ),
+            ),
+            _buildListBooks(),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Text(
+                'Сейчас в тренде',
+                style: widget.themeNotifier.getTheme().textTheme.headline6,
+              ),
+            ),
+            _buildListBooks(),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _buildListBooks() {
+    return Container(
+      height: 250,
+      alignment: Alignment.centerLeft,
+      padding: const EdgeInsets.only(top: 20),
+      child: ListView.builder(
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: 5,
+        itemBuilder: (BuildContext context, int index) {
+          return Row(
+            children: [
+              const SizedBox(
+                width: 30,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: const Image(
+                          height: 190,
+                          image: AssetImage(
+                              'assets/images/main_page/book_image.jpg'))),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Кот в шляпе',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  const Text(
+                    'Доктор Сьюз',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+              if (index == genres.length - 1)
+                const SizedBox(
+                  width: 30,
+                ),
+            ],
+          );
+      }),
     );
   }
 }
