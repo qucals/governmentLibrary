939e5415ad861616979d41eb291ae0495e27a15a
qucals <totalboy@outlook.com>
2022-03-31 19:58:49 +0500

    Написал эффект загрузки
    Signed-off-by: qucals <totalboy@outlook.com>

--- lib/src/main_page/main_page.dart
+++ lib/src/main_page/main_page.dart
@@ -14,11 +14,14 @@
  limitations under the License.
  */
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:government_library/src/components/bottom_bar.dart';
 import 'package:group_button/group_button.dart';
+import 'package:shimmer/shimmer.dart';
 
+import 'package:government_library/src/components/bottom_bar.dart';
 import 'package:government_library/src/components/text_button.dart';
 import 'package:government_library/src/components/text_field.dart';
 import 'package:government_library/src/theming/theme_manager.dart';
@@ -35,6 +38,7 @@ class LibraryMainPage extends StatefulWidget {
 
 class _LibraryMainPageState extends State<LibraryMainPage> {
   int _currentPageIndex = 0;
+  bool isLoading = true;
 
   final TextEditingController _search = TextEditingController();
   final GroupButtonController _genres = GroupButtonController(selectedIndex: 0);
@@ -184,25 +188,29 @@ class _LibraryMainPageState extends State<LibraryMainPage> {
             const SizedBox(
               height: 20,
             ),
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: GroupButton(
-                onSelected: (index, isSelected) => setState(() {
-                  selectedGenre = genres[index];
-                }),
-                buttons: genres,
-                controller: _genres,
-                options: GroupButtonOptions(
-                  spacing: 10,
-                  runSpacing: 5,
-                  borderRadius: BorderRadius.circular(10),
-                  selectedColor:
-                      widget.themeNotifier.getTheme().colorScheme.secondary,
-                  unselectedColor: const Color(0xFFE5E5EA),
-                  mainGroupAlignment: MainGroupAlignment.start,
-                  selectedShadow: [],
-                  unselectedShadow: [],
+            Shimmer.fromColors(
+              baseColor: Colors.grey[200]!,
+              highlightColor: Colors.grey[350]!,
+              child: Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: GroupButton(
+                  onSelected: (index, isSelected) => setState(() {
+                    selectedGenre = genres[index];
+                  }),
+                  buttons: genres,
+                  controller: _genres,
+                  options: GroupButtonOptions(
+                    spacing: 10,
+                    runSpacing: 5,
+                    borderRadius: BorderRadius.circular(10),
+                    selectedColor:
+                        widget.themeNotifier.getTheme().colorScheme.secondary,
+                    unselectedColor: const Color(0xFFE5E5EA),
+                    mainGroupAlignment: MainGroupAlignment.start,
+                    selectedShadow: [],
+                    unselectedShadow: [],
+                  ),
                 ),
               ),
             ),
@@ -217,7 +225,7 @@ class _LibraryMainPageState extends State<LibraryMainPage> {
                 style: widget.themeNotifier.getTheme().textTheme.headline6,
               ),
             ),
-            _buildListBooks(),
+            _buildListBooks(isLoading),
             const SizedBox(
               height: 30,
             ),
@@ -229,65 +237,70 @@ class _LibraryMainPageState extends State<LibraryMainPage> {
                 style: widget.themeNotifier.getTheme().textTheme.headline6,
               ),
             ),
-            _buildListBooks(),
+            _buildListBooks(isLoading),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildListBooks() {
+  Widget _buildListBooks(bool isLoading) {
     return Container(
       height: 250,
       alignment: Alignment.centerLeft,
       padding: const EdgeInsets.only(top: 20),
       child: ListView.builder(
-        shrinkWrap: true,
-        physics: const ClampingScrollPhysics(),
-        scrollDirection: Axis.horizontal,
-        itemCount: 5,
-        itemBuilder: (BuildContext context, int index) {
-          return Row(
-            children: [
-              const SizedBox(
-                width: 30,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: const Image(
-                          height: 190,
-                          image: AssetImage(
-                              'assets/images/main_page/book_image.jpg'))),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    'Кот в шляпе',
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
+          shrinkWrap: true,
+          physics: const ClampingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return Shimmer.fromColors(
+                baseColor: Colors.grey[200]!,
+                highlightColor: Colors.grey[350]!,
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 30,
                     ),
-                  ),
-                  const SizedBox(height: 5),
-                  const Text(
-                    'Доктор Сьюз',
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.grey,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: const Image(
+                                height: 190,
+                                image: AssetImage(
+                                    'assets/images/main_page/book_image.jpg'))),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        if (!isLoading)
+                          const Text(
+                            'Кот в шляпе',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        const SizedBox(height: 5),
+                        if (!isLoading)
+                          const Text(
+                            'Доктор Сьюз',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),
+                          ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-              if (index == genres.length - 1)
-                const SizedBox(
-                  width: 30,
-                ),
-            ],
-          );
-      }),
+                    if (index == genres.length - 1)
+                      const SizedBox(
+                        width: 30,
+                      ),
+                  ],
+                ));
+          }),
     );
   }
 }
